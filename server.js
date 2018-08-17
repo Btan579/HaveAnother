3ff72a0095f75ff283cad478b8c03fc5d03f280a
Brendan Tanner <btan579@gmail.com>
2018-08-16 21:14:55 -0500

    "edited server"

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const passport = require('passport');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 
-const {
-    router: usersRouter
-} = require('./users');
-const {
-    router: authRouter,
-    localStrategy,
-    jwtStrategy
-} = require('./auth');
+// const {
+//     router: usersRouter
+// } = require('./users');
+// const {
+//     router: authRouter,
+//     localStrategy,
+//     jwtStrategy
+// } = require('./auth');
 
 const app = express();
 
@@ -21,37 +21,37 @@ app.use(morgan('common'));
 
 
 
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-    if (req.method === 'OPTIONS') {
-        return res.send(204);
-    }
-    next();
-});
+// app.use(function (req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+//     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+//     if (req.method === 'OPTIONS') {
+//         return res.send(204);
+//     }
+//     next();
+// });
 
-passport.use(localStrategy);
-passport.use(jwtStrategy);
+// passport.use(localStrategy);
+// passport.use(jwtStrategy);
 
-app.use('/api/users/', usersRouter);
-app.use('/api/auth/', authRouter);
+// app.use('/api/users/', usersRouter);
+// app.use('/api/auth/', authRouter);
 
-const jwtAuth = passport.authenticate('jwt', {
-    session: false
-});
+// const jwtAuth = passport.authenticate('jwt', {
+//     session: false
+// });
 
-app.get("/", () => {
-    console.log("GET request to /");
-    res.send("home page");
+// app.get("/", () => {
+//     console.log("GET request to /");
+//     res.send("home page");
     
-});
+// });
 app.listen(process.env.PORT || 8080);
 
 
 
-module.exports = {
-    app,
-    runServer,
-    closeServer
-};
+// module.exports = {
+//     app,
+//     runServer,
+//     closeServer
+// };
